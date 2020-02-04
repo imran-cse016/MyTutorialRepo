cdf517107ba351c9b5ca734fac5a970ecbff155c
imran <imran.cse016@outlook.com>
2020-02-05 01:59:16 +0530

    Add project files.

---
+++ MyTutorialClientApp/Model/ToDoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTutorialClientApp.Model
+{
+    public class ToDoModel
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
